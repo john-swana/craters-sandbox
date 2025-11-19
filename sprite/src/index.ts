5a0d31f7515d30022b27ce2ea0d8eecf7b5356b3
john-swana <johnswana49@gmail.com>
2025-11-19 20:07:30 +0000

    Deploying to gh-pages from @ john-swana/craters-sandbox@8a148c4591ddc907b83c6dc17883d2046d641144 🚀

--- sprite/src/index.ts
+++ sprite/src/index.ts
@@ -12,9 +12,27 @@ import CRATERS from "craters"
                 [1, 1],
             ]
             const sprite = new CRATERS.Sprite(canvas2DRenderer, spritesheet, 214, 282, frames, 1 / 25, 107, 140)
+            
+            // Input setup
+            const input = new CRATERS.Input();
+            input.bind(CRATERS.Input.KEY.LEFT_ARROW, "LEFT");
+            input.bind(CRATERS.Input.KEY.RIGHT_ARROW, "RIGHT");
+            input.bind(CRATERS.Input.KEY.UP_ARROW, "UP");
+            input.bind(CRATERS.Input.KEY.DOWN_ARROW, "DOWN");
+
+            let posX = 100;
+            let posY = 100;
+            const speed = 4;
+
             const renderLoop = new CRATERS.RenderLoop(function(renderLoop: any) {
+                // Update Position
+                if (input.isPressed("LEFT")) posX -= speed;
+                if (input.isPressed("RIGHT")) posX += speed;
+                if (input.isPressed("UP")) posY -= speed;
+                if (input.isPressed("DOWN")) posY += speed;
+
                 canvas2DRenderer.clear()
-                sprite.draw(100, 100, renderLoop)
+                sprite.draw(posX, posY, renderLoop)
             })
             document.body.append(canvas2DRenderer.canvasElement)
         })
